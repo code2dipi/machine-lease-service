2eeb42362bbee7e705f0e28c1cd41edfc82c456b
Dipi <dipineupane@gmail.com>
2024-02-04 21:31:29 +0100

    changes in Equipment controller...

--- src/main/java/com/dipi/machineleaseservice/repository/EquipmentRepository.java
+++ src/main/java/com/dipi/machineleaseservice/repository/EquipmentRepository.java
@@ -4,7 +4,7 @@ import com.dipi.machineleaseservice.model.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "equipments", path = "equipments")
+//@RepositoryRestResource(collectionResourceRel = "equipments", path = "equipments")
 
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
 }
